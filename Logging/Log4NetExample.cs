ba16032c145f79add5bef5441cac7c7772242188
anonimen14 <anonimen14@gmail.com>
2013-05-14 19:50:52 +0300

    commit1

---
+++ Logging/Log4NetExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using log4net;
+
+namespace Logging
+{
+    class Log4NetExample
+    {
+        static void Main()
+        {
+            log4net.Config.BasicConfigurator.Configure();
+
+            ILog log = log4net.LogManager.GetLogger(typeof(Log4NetExample));
+            ILog debugLog = LogManager.GetLogger("Debug");
+            ILog exceptionLog = LogManager.GetLogger("Exception");
+
+            log.Debug("Bla bla bla ...Debug!");
+            log.Info("How beautyful the console looks like");
+            log.Warn("You are great you did this");
+            log.Error("Pesho-Gosho Error");
+            log.Fatal("sadi the great");
+
+            debugLog.Info("Press key:");
+            string input = Console.ReadLine();
+            try
+            {
+                int n = int.Parse(input);
+            }
+            catch (Exception ex) 
+            {
+                exceptionLog.Error(ex.Message);
+            }
+        }
+    }
+}
---
+++ T4TemplateExample/TemplateClass.cs
@@ -0,0 +1,16 @@
+﻿namespace T4TemplateExample
+{
+	class TemplateClass
+	{
+		public int Item0 { get; set; }
+		public int Item1 { get; set; }
+		public int Item2 { get; set; }
+		
+	// Directory content:
+	// "App.config"
+	// "T4TemplateExample.csproj"
+	// "TemplateClass.cs"
+	// "TemplateClass.tt"
+	//END Directory content!
+	}
+}
